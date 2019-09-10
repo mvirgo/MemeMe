d987728620c98ae7a471e9f3c18da08507741ef3
mvirgo <mvirgs44@gmail.com>
2019-09-09 21:31:10 -0700

    feat: meme text overlays image

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var topTextField: UITextField!
     @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var topTextConstraint: NSLayoutConstraint!
+    @IBOutlet weak var bottomTextConstraint: NSLayoutConstraint!
     
     // MARK: Text Field Delegate objects
     let memeTextDelegate = MemeTextFieldDelegate()
@@ -35,18 +37,47 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,
         bottomTextField.defaultTextAttributes = memeTextDelegate.memeTextAttributes
     }
     
+    override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
+        // If there's an image, calculate and set the text
+        if let image = imagePickerView.image {
+            setImageText(image)
+        }
+    }
+    
     // MARK: UIImagePicker Delegate functions
     func imagePickerController(
         _ picker: UIImagePickerController,
         didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            // Set the image into the view
             imagePickerView.image = image
+            // Set the image text
+            setImageText(image)
         }
         dismiss(animated: true, completion: nil)
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: Image Text helper functions
+    func setImageText(_ image: UIImage) {
+        // Get the image dimensions and scale
+        let imageViewHeight = imagePickerView.bounds.height
+        let imageViewWidth = imagePickerView.bounds.width
+        let imageHeight = imagePickerView.intrinsicContentSize.height
+        let imageWidth = imagePickerView.intrinsicContentSize.width
+        var scale: CGFloat = 0.0
+        if UIDevice.current.orientation.isPortrait {
+            scale = imageViewWidth / imageWidth
+        } else {
+            scale = imageViewHeight / imageHeight
+        }
+        let middle = imageViewHeight / 2
+        // Move the meme text onto the image
+        topTextConstraint.constant = middle - (scale * (imageHeight / 2))
+        bottomTextConstraint.constant = middle - (scale * (imageHeight / 2))
     }
 
     // MARK: Actions
