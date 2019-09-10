b0b6e215cd4bed2576491e8c2eb740b108a63417
mvirgo <mvirgs44@gmail.com>
2019-09-10 09:58:06 -0700

    feat: move view when keyboard appears Known issue: Keyboard may still cover text depending on screen size.

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -23,6 +23,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,
     let memeTextDelegate = MemeTextFieldDelegate()
     
     // MARK: View functions
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        subscribeToKeyboardNotifications()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
@@ -42,6 +47,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,
         if let image = imagePickerView.image {
             setImageText(image)
         }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
     }
     
     // MARK: UIImagePicker Delegate functions
@@ -78,6 +88,36 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,
         // Move the meme text onto the image
         topTextConstraint.constant = middle - (scale * (imageHeight / 2))
         bottomTextConstraint.constant = middle - (scale * (imageHeight / 2))
+    }
+    
+    // MARK: Keyboard functions to avoid overlaying onto text
+    // Keyboard functions are from Udacity classroom
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(
+            self, selector: #selector(keyboardWillShow(_:)),
+            name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(
+            self, selector: #selector(keyboardWillHide(_:)),
+            name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(
+            self, name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+    
+    @objc func keyboardWillHide(_ notification:Notification) {
+        view.frame.origin.y = 0
+    }
+    
+    @objc func keyboardWillShow(_ notification:Notification) {
+        view.frame.origin.y = -getKeyboardHeight(notification)
+    }
+    
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
     }
 
     // MARK: Actions
